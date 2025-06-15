be596d9d71a98d19d3b9e31a619577b1219e268a
caiquefrd <caique.fred@outlook.com>
2025-06-15 16:00:47 -0300

    containerizing

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,10 +2,10 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
   user: 'lynch_admin',
-  host: 'localhost',
+  host: 'lynchareadb',
   database: 'lynchareadb',
   password: '123',
-  port: 5432
+  port: 5437
 });
 
 export default pool;
